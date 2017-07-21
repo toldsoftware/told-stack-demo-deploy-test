c05c2cfa6092797d001c9696091fe7370dc0dcc0
Rick Love <rick@toldpro.com>
2017-07-20 22:48:02 -0500

    Improve Logging

--- stripe-checkout-02-process/bundle.js
+++ stripe-checkout-02-process/bundle.js
@@ -81402,10 +81402,11 @@ exports.runFunction = function_builder_1.build_runFunction_common(buildFunction,
     context.log('START');
     const q = context.bindings.inProcessQueueTrigger;
     const saveData = (data) => __awaiter(this, void 0, void 0, function* () {
-        context.log('Processing', { paymentpaymentStatus: data.paymentStatus, data });
         // Log History
         const b = config.getBinding_stripeCheckoutTable_fromTrigger(q);
-        yield exports.deps.saveEntity(b.tableName, b.partitionKey, `${b.rowKey}_at-${Date.now()}`, Object.assign({}, data, { isLog: true }));
+        const changedRowKey = `${b.rowKey}_at-${Date.now()}`;
+        context.log('saveData', { paymentStatus: data.paymentStatus, data, b });
+        yield exports.deps.saveEntity(b.tableName, b.partitionKey, changedRowKey, Object.assign({}, data, { isLog: true }));
         // Save Main
         yield exports.deps.saveEntity(b.tableName, b.partitionKey, b.rowKey, data);
     });
