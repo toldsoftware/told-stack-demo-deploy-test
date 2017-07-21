5ab6b9dd8b7b9bb64475abc778ad77ab37e43eca
Rick Love <rick@toldpro.com>
2017-07-20 23:52:37 -0500

    Add Log Prefix to Not Interfere with Main Row Key

--- stripe-checkout-02-process/bundle.js
+++ stripe-checkout-02-process/bundle.js
@@ -81408,7 +81408,7 @@ exports.runFunction = function_builder_1.build_runFunction_common(buildFunction,
         try {
             // Log History
             const b = config.getBinding_stripeCheckoutTable_fromTrigger(q);
-            const changedRowKey = `${b.rowKey}_at-${Date.now()}`;
+            const changedRowKey = `log_${b.rowKey}_at-${Date.now()}`;
             // context.log('saveData START', { paymentStatus: data.paymentStatus, data, b, changedRowKey });
             yield exports.deps.saveEntity(b.tableName, b.partitionKey, changedRowKey, Object.assign({}, data, { isLog: true }));
             // Save Main
